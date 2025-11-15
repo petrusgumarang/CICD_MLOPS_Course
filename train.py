5c12ea2145019fb3a95d51c7b29b26940bf9f27d
Petrus HK Gumarang <petrusgumarang@gmail.com>
2025-11-16 00:58:00 +0700

    update to 70x

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ print("Training v2")
 # Contoh training data untuk demo: y = 2x
 data = pd.DataFrame({
     "x": [1, 2, 3, 4, 5],
-    "y": [50, 100, 150, 200, 250]  # 50x
+    "y": [70, 140, 210, 280, 350]  # 50x
 })
 
 X = data[["x"]]
